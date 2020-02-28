04ced1883689ae9796b6a1a3678515674a912029
Devki1 <gdev3195@gmail.com>
2020-02-28 16:38:06 +0530

    ADDED:Player rolls the die to get a number between 1 to 6

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -3,4 +3,5 @@ echo "Welcome to snake and ladder Simulation"
 #snake and ladder game played with single player and  start with zero position
 numberofPlayer=1
 position=0
+playerRollsDie=$((RANDOM%6+1))
 
