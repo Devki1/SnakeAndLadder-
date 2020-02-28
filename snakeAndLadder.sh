73be08317d4d5ea4b20b0faaae1da9f3c26a5251
Devki1 <gdev3195@gmail.com>
2020-02-28 19:15:56 +0530

    ADDED:Winning position

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -6,18 +6,26 @@ position=0
 DontPlay=0
 Ladder=1
 snake=2
-playerRollsDie=$((RANDOM%6+1))
-checkOption=$((RANDOM%3))
-case $checkOption in
-	$DontPlay)
+while(($position!=100))
+do
+	playerRollsDie=$((RANDOM%6+1))
+	checkOption=$((RANDOM%3))
+	case $checkOption in
+		$DontPlay)
 			position=$position
 			echo "Stay in the same $position"
 			;;
-	$Ladder)
+		$Ladder)
 			position=$(($position+$playerRollsDie))
 			echo "counted a ladder position:"$position
 			;;
-	$snake)
+		$snake)
 			position=$(($position-$playerRollsDie))
-			echo "count a new snake position:" $position 
-esac
+			echo "new position:"$position
+			if(($position<0))
+			then
+				position=0
+			fi
+			;;
+	esac
+done
